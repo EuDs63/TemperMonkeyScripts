6283213966a9f0f5f30a37db57ca71daa002ace4
EuDs63 <erica23187@gmail.com>
2024-10-04 15:24:00 +0800

    feat: 1.3 Make links from youtube,x,tieba simpler

--- Copy Web Title and Url.user.js
+++ Copy Web Title and Url.user.js
@@ -1,25 +1,20 @@
 // ==UserScript==
 // @name         Copy Web Title and Url
 // @namespace    http://ds63.eu.org
-// @version      1.2
+// @version      1.3
 // @description  Copy Web Title and Url to clipboard when Alt + S is pressed.
 // @include      *
 // @grant        none
 // ==/UserScript==
 
-// 监听键盘事件
-window.addEventListener("keydown", function(event) {
-    // 检查是否按下Alt键（event.altKey）和S键（event.key === "s"）
+window.addEventListener("keydown", function (event) {
     if (event.altKey && event.key === "s") {
-        // 阻止默认行为，防止复制操作
         event.preventDefault();
 
-        // 复制特殊文本到剪贴板
         copyToClipboard(getText());
     }
 });
 
-// 复制文本到剪贴板的函数
 function copyToClipboard(text) {
     var dummyInput = document.createElement("input");
     document.body.appendChild(dummyInput);
@@ -29,23 +24,47 @@ function copyToClipboard(text) {
     document.body.removeChild(dummyInput);
 }
 
-// 得到网站标题和网址
-function getText(){
+function getText() {
     var pageTitle = document.title;
     var currentUrl = window.location.href;
-    // 对得到的网址进行处理
-    // 如果是b站的链接，
-    if(currentUrl.indexOf("bilibili.com") != -1){
-        // 判断是否有_哔哩哔哩_bilibili
-        if (pageTitle.endsWith("_哔哩哔哩_bilibili")){
-            // 删去title最后的"_哔哩哔哩_bilibili"
+
+    // for bilibili.com
+    if (currentUrl.indexOf("bilibili.com") != -1) {
+        // check if the title ends with "_哔哩哔哩_bilibili"
+        if (pageTitle.endsWith("_哔哩哔哩_bilibili")) {
+            // delete the last 14 characters
             pageTitle = pageTitle.substring(0, pageTitle.length - 14);
         }
-        
-        //截去后面的参数
+
+        // get rid of the query string
         currentUrl = currentUrl.split("?")[0];
     }
 
+    // for youtube.com
+    if (currentUrl.indexOf("youtube.com") != -1) {
+        // check if the title begins with (number)
+        if (pageTitle.match(/^\(\d+\)/)) {
+            // delete the first (number)
+            pageTitle = pageTitle.replace(/^\(\d+\)/, "");
+        }
+    }
+
+    // for tieba.baidu.com
+    if (currentUrl.indexOf("tieba.baidu.com") != -1) {
+        // get rid of the query string
+        currentUrl = currentUrl.split("?")[0];
+    }
+
+    // for x.com
+    if (currentUrl.indexOf("x.com") != -1) {
+        // twitter will make the title with the format "username on X: content"
+        // I prefer to keep the beginning part only, that is "username on X"
+        if (pageTitle.indexOf(":") != -1) {
+            pageTitle = pageTitle.split(":")[0];
+        }
+    }
+
+    // the " " is a patch for the stubid qq link parse
     var formattedText = `[${pageTitle}](${currentUrl} )`;
 
     return formattedText;
--- README.md
+++ README.md
@@ -13,16 +13,15 @@
 
 **1.2 Update:** Make links from bilibili simpler
 
-**todo**
-- [ ] The `s` is conflict with Youtube,so this script does not work in Youtube. I still can not find ways to solve it.
+**1.3 Update:** Make links from youtube,x,tieba simpler
 
+**Todo**
+- [x] The `s` is conflict with Youtube,so this script does not work in Youtube. ~~I still can not find ways to solve it.~~ You can click the input bar, and then press the `Alt` and `s` to use the script
 
 ### 自动跳转链接
 **Usage:** This script automatically redirects links to their translated versions without requiring any user input.
 
 **PS:** There is a better designed and tested script: [OldPanda/Open-the-F-king-URL-Right-Now: 自动跳转某些网站不希望用户直达的外链](https://github.com/OldPanda/Open-the-F-king-URL-Right-Now)
 
-**Tode**
+**Todo**
 - [x] 目前的b站标题处理过于粗暴
-
-
